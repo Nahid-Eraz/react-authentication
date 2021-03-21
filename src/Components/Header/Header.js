bf9c8ed33ab4059bd9d4ab54aae3849ff7a54161
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-03-21 23:12:55 +0600

    done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 import { UserContext } from '../../App';
 
 const Header = () => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [loggedInUser] = useContext(UserContext);
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
@@ -14,12 +14,12 @@ const Header = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav nav-text">
-                        {/* <p className="nav-link">{loggedInUser.name}</p> */}
                         <Link className="nav-link" to="/home">Home</Link>
                         <Link className="nav-link" to="/destination">Destination</Link>
                         <Link className="nav-link" to="/blog">Blog</Link>
                         <Link className="nav-link" to="/contact">Contact</Link>
                         <Link className="nav-link" to="/signin">Sign In</Link>
+                        <p className="nav-link">{loggedInUser.name}</p>
                     </div>
                 </div>
             </div>
